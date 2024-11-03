85488419386fc2b4e8d780a240542316ebfc12ce
Juan Jose Arenas <juanjoseaq99@gmail.com>
2024-11-02 19:42:54 -0500

    Asignacion del apikey de Sendgrid al secreto SENDGRIDAPYKEY y el correo remitente tambien al secreto CorreoFrom

--- src/main/java/co/edu/uco/ucobet/generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
+++ src/main/java/co/edu/uco/ucobet/generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
@@ -1,7 +1,9 @@
+
 package co.edu.uco.ucobet.generales.infrastructure.primaryadapters.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+
 import co.edu.uco.ucobet.generales.aplication.primaryports.dto.CityDTO;
 import co.edu.uco.ucobet.generales.aplication.primaryports.dto.RegisterNewCityDTO;
 import co.edu.uco.ucobet.generales.aplication.primaryports.interactor.city.RegisterNewCityInteractor;
@@ -29,87 +32,89 @@ import co.edu.uco.ucobet.generales.infrastructure.primaryadapters.controller.res
 @RestController
 @RequestMapping("/general/api/v1/cities")
 public class RegisterNewCityController {
+	@Value("${sendgrid.api.key}")
+	private String apiKey;
 
-    private final RegisterNewCityInteractor registerNewCityInteractor;
-    private final RetrieveCityInteractor retrieveCityInteractor;
-    private static final String API_KEY = "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";
+	@Value("${sendgrid.from}")
+	private String correofrom;
 
-    public RegisterNewCityController(final RegisterNewCityInteractor registerNewCityInteractor,
-            final RetrieveCityInteractor retrieveCityInteractor) {
-        this.registerNewCityInteractor = registerNewCityInteractor;
-        this.retrieveCityInteractor = retrieveCityInteractor;
-    }
+	private final RegisterNewCityInteractor registerNewCityInteractor;
+	private final RetrieveCityInteractor retrieveCityInteractor;
 
-    @PostMapping
-    public ResponseEntity<CityResponse> crear(@RequestBody RegisterNewCityDTO dto) {
-        var httpStatusCode = HttpStatus.CREATED;
-        var ciudadResponse = new CityResponse();
+	public RegisterNewCityController(final RegisterNewCityInteractor registerNewCityInteractor,
+			final RetrieveCityInteractor retrieveCityInteractor) {
+		this.registerNewCityInteractor = registerNewCityInteractor;
+		this.retrieveCityInteractor = retrieveCityInteractor;
+	}
 
-        try {
-            registerNewCityInteractor.execute(dto);
-            ciudadResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00008));
-            
-            sendEmailNotification(dto.getCityName());
+	@PostMapping
+	public ResponseEntity<CityResponse> crear(@RequestBody RegisterNewCityDTO dto) {
+		var httpStatusCode = HttpStatus.CREATED;
+		var ciudadResponse = new CityResponse();
 
-        } catch (final UcobetException excepcion) {
-            httpStatusCode = HttpStatus.BAD_REQUEST;
-            ciudadResponse.getMensajes().add(excepcion.getUserMessage());
+		try {
+			registerNewCityInteractor.execute(dto);
+			ciudadResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00008));
 
-        } catch (final Exception excepcion) {
-            httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-            var mensajeUsuario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00009);
-            ciudadResponse.getMensajes().add(mensajeUsuario);
-        }
+			sendEmailNotification(dto.getCityName());
 
-        return new ResponseEntity<>(ciudadResponse, httpStatusCode);
-    }
-	
+		} catch (final UcobetException excepcion) {
+			httpStatusCode = HttpStatus.BAD_REQUEST;
+			ciudadResponse.getMensajes().add(excepcion.getUserMessage());
+
+		} catch (final Exception excepcion) {
+			httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+			var mensajeUsuario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00009);
+			ciudadResponse.getMensajes().add(mensajeUsuario);
+		}
+
+		return new ResponseEntity<>(ciudadResponse, httpStatusCode);
+	}
+
 	@GetMapping
-	public ResponseEntity<CityResponse> consultar(){
+	public ResponseEntity<CityResponse> consultar() {
 		var httpStatusCode = HttpStatus.ACCEPTED;
 		var cityResponse = new CityResponse();
-	try {
-		var cityDto = CityDTO.create();
-		cityResponse.setDatos(retrieveCityInteractor.execute(cityDto));
-		cityResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00013));
-	}catch (final UcobetException exception) {
-		httpStatusCode = HttpStatus.BAD_REQUEST;
-		cityResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00014));
-		exception.printStackTrace();
-		
-	}catch(final Exception exception) {
-		httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-		var mensajeUsusario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00015);
-		cityResponse.getMensajes().add(mensajeUsusario);
-		exception.printStackTrace();
-	}
-	return new ResponseEntity<>(cityResponse, httpStatusCode);
-	}
-	
-    private void sendEmailNotification(String cityName) {
-        Email from = new Email("juanjoseaq99@gmail.com");
-        String subject = "Nueva Ciudad Registrada";
-        Email to = new Email("juanjoseaq99@gmail.com");
-        
-        String mensajeFormateado = String.format(
-                MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00016),
-                cityName
-            );
-            
-        Content content = new Content("text/html", "<strong>" + mensajeFormateado + cityName+ "</strong>");
-        Mail mail = new Mail(from, subject, to, content);
+		try {
+			var cityDto = CityDTO.create();
+			cityResponse.setDatos(retrieveCityInteractor.execute(cityDto));
+			cityResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00013));
+		} catch (final UcobetException exception) {
+			httpStatusCode = HttpStatus.BAD_REQUEST;
+			cityResponse.getMensajes().add(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00014));
+			exception.printStackTrace();
 
-        SendGrid sg = new SendGrid(API_KEY);
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println("Email enviado - Status Code: " + response.getStatusCode());
-        } catch (IOException ex) {
-            System.err.println("Error al enviar email: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
+		} catch (final Exception exception) {
+			httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+			var mensajeUsusario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00015);
+			cityResponse.getMensajes().add(mensajeUsusario);
+			exception.printStackTrace();
+		}
+		return new ResponseEntity<>(cityResponse, httpStatusCode);
+	}
+
+	private void sendEmailNotification(String cityName) {
+		Email from = new Email(correofrom);
+		String subject = "Nueva Ciudad Registrada";
+		Email to = new Email("juanjoseaq99@gmail.com");
+
+		String mensajeFormateado = String.format(MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00016),
+				cityName);
+
+		Content content = new Content("text/html", mensajeFormateado + "<strong>" + cityName + "</strong>");
+		Mail mail = new Mail(from, subject, to, content);
+
+		SendGrid sg = new SendGrid(apiKey);
+		Request request = new Request();
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println("Email enviado - Status Code: " + response.getStatusCode());
+		} catch (IOException ex) {
+			System.err.println("Error al enviar email: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
 }
